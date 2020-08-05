1e4cf31cfb1fc35956fd7701cc0a3059454ae1b8
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-08-05 18:29:18 -0300

    finalizando o back end

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+
+import routes from './routes'
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.json({
-    name: 'Evandro',
-    emial: 'vando@gmail.com'
-  })
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-app.listen(3000, () => console.log("Servidor no ar"));
+
+app.listen(3333, () => console.log("Servidor no ar"));
---
+++ src/util/convertHourToMinutes.ts
@@ -0,0 +1,6 @@
+export default function converteHourToMinutes(time: string) {
+  const [hour, minutes] = time.split(':').map(Number);
+  const timeInMinutes = (hour * 60) + minutes;
+
+  return timeInMinutes;
+}
